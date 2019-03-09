4de2a97f17cbc05bc1a1485c3482239f7da65813
David Thayer <dthayer16@gmail.com>
2019-03-09 00:31:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,5 +26,10 @@ def bulk_tweet_shortener(array)
   end
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
 end
